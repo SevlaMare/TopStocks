42f75d502b3fb4c9505a5a1e7ecffc819c621971
Mare <sevilamare@gmail.com>
2020-10-10 12:56:33 -0300

    render serie

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -2,36 +2,33 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
-let renderDetail = () => {
-  if (data.length > 0)
-    return (
-      data[0].map( (data, symbol) => (
-        <>
-          <Stock key={data.id}
-            title={data.symbol}
-            close={data.historical[0].close}
-            change={data.historical[0].changePercent}
-          />
+let renderDetail = (data) => {
+  return (
+    data[0].map( (data, symbol) => (
+      <>
+        <p>Company: {data.symbol}</p>
+        { data.historical.map(item => {
+          return <p>{item.date}Serie x</p>
+        }) }
 
-          <li key={data.id}>
-            <Link to={`${url}/${data.symbol}`}>{data.symbol}</Link>
-          </li>
-        </>
-      ))
-    )
+        <p>Open {data.historical[0].open}</p>
+        <p>Close {data.historical[0].close}</p>
 
-  return null;
+        <p>Change {data.historical[0].changePercent}</p>
+        <hr></hr>
+      </>
+    ))
+  )
 }
 
 function Detail() {
   let { stockId } = useParams(); // get end of url
   const data = useSelector(state => state.data); // map state
 
-  console.log('state from detail', data)
-
   return (
     <div className="detail">
       <h3>{stockId}</h3>
+      { renderDetail(data) }
     </div>
   );
 }
