fe8e07ab108a32a0c37ca1b26f68b4dadcd69b6d
Mare <sevilamare@gmail.com>
2020-10-11 11:54:22 -0300

    query details

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -2,23 +2,27 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
-let renderDetail = (data) => (
-  data[0].map( (data, symbol) => (
-    <>
-      <p>Company: {data.symbol}</p>
-      { data.historical.map(item => {
-        return (
-          <>
-            <p>{item.date}</p>
-            <p>{item.changePercent.toFixed(2)}% {item.open.toFixed(2)} {item.close.toFixed(2)}</p>
-            <hr></hr>
-          </>
-        )
-      }) }
-      <hr></hr>
-    </>
-  ))
-)
+let renderDetail = (data, stockId) => {
+  const find = data[0].filter(element => element.symbol === 'GOOG');
+
+  return (
+    find.map( data => (
+      <>
+        <p>Company: {data.symbol}</p>
+        { data.historical.map(item => {
+          return (
+            <>
+              <p>{item.date}</p>
+              <p>{item.changePercent.toFixed(2)}% {item.open.toFixed(2)} {item.close.toFixed(2)}</p>
+              <hr></hr>
+            </>
+          )
+        }) }
+        <hr></hr>
+      </>
+    ))
+  );
+};
 
 function Detail() {
   let { stockId } = useParams(); // get end of url
@@ -27,7 +31,7 @@ function Detail() {
   return (
     <div className="detail">
       <h3>{stockId}</h3>
-      { renderDetail(data) }
+      { renderDetail(data, stockId) }
     </div>
   );
 }
