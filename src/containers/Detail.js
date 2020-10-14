47ffabb37ebaf22c296c624fca0df80a6359d1a8
Mare <sevilamare@gmail.com>
2020-10-14 10:57:55 -0300

    detail refactor

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -2,37 +2,32 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const renderDetail = (data, stockId) => {
-  const find = data.base.filter(element => element.symbol === stockId);
-
-  return (
-    find.map(data => {
-      return (
-        <>
-          <p> Company: {data.symbol} </p>
-
-          { data.historical.map(item => (
-            <>
-              <p> Day {item.date} </p>
-              <p> Change {item.changePercent.toFixed(2)} % </p>
-              <p> Open  {item.open.toFixed(2)} </p>
-              <p> Close {item.close.toFixed(2)} </p>
-            </>
-          )) }
-        </>
-      )
-    })
-  );
-};
-
 function Detail() {
   const { stockId } = useParams(); // get end of url
   const data = useSelector(state => state.dataR); // map state
+  const find = data.base.filter(element => element.symbol === stockId);
 
   return (
-    <div className="detail">
-      <h3 className="title">{stockId}</h3>
-      { renderDetail(data, stockId) }
+    <div>
+      { find.map(data => {
+        return (
+          <div>
+            <h3 className="title">{stockId}</h3>
+            <p> Company: {stockId} </p>
+
+            { data.historical.map(item => {
+              return (
+                <div>
+                  <p> Day {item.date} </p>
+                  <p> Change {item.changePercent.toFixed(2)} % </p>
+                  <p> Open  {item.open.toFixed(2)} </p>
+                  <p> Close {item.close.toFixed(2)} </p>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   );
 }
