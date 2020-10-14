3941c13363e29c91bffaccca26cf7cd5589521dd
Mare <sevilamare@gmail.com>
2020-10-14 17:31:43 -0300

    stock test

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { render, screen } from '@testing-library/react';
+
+import App from '../src/components/About';
+
+it('render start app', () => {
+  render(<App></App>)
+})
--- test/stock.test.js
+++ test/stock.test.js
@@ -4,12 +4,7 @@ import ReactDOM from 'react-dom';
 import { toBeInTheDocument } from '@testing-library/jest-dom'; // assertions
 import { render, screen } from '@testing-library/react';
 
-import App from '../src/components/About';
 import Stock from '../src/components/Stock';
-
-it('render start app', () => {
-  render(<App></App>)
-})
 
 it('accept valid props to render', () => {
   render(<Stock
@@ -19,7 +14,7 @@ it('accept valid props to render', () => {
   />)
 })
 
-test('elements are embed correctly', () => {
+it('elements are embed correctly', () => {
   const checkString = 'AFK';
 
   render(<Stock close={123} change={1.23} link={checkString} />);
